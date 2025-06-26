e4058731cb07d21ebcb7274339b5c94fed8d4bbe
MANAV PRITESH DESAI <23ucs639@lnmiit.ac.in>
2025-06-26 21:10:23 +0000

    where is sidebar

--- src/components/side-panel/SidePanel.jsx
+++ src/components/side-panel/SidePanel.jsx
@@ -19,7 +19,7 @@ import {
 
 export default function SidePanel() {
   const { connected, client } = useLiveAPIContext();
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const loggerRef = useRef(null);
   const loggerLastHeightRef = useRef(-1);
   const { log, logs } = useLoggerStore();
